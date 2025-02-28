6daee702f3a73ecdccabe0d3588276b8bf7e11a9
DEV SRIWARARAK <devsriwararak.work@gmail.com>
2025-02-28 23:20:13 +0700

    28-2-2568-v-2

--- src/app.js
+++ src/app.js
@@ -29,7 +29,12 @@ const app = express();
 // Webhook Route ห้ามย้ายตำแหน่ง
 // app.post('/api/webhook', express.raw({ type: "application/json" }),stripeHookCheck )
 
-app.use(cors({ origin: "http://localhost:5173" }));
+app.use(cors({
+  origin: "http://localhost:5173",
+  methods: "GET, POST, PUT, DELETE, OPTIONS",
+  allowedHeaders: "Content-Type, Authorization"
+}));
+
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true }));
 app.get("/", (req, res) => {
